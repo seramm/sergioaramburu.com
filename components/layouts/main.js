d6feb4a12e864337dadc3ec0aa147bc6b392cea6
seramm <sergioaramburu@tutanota.com>
2024-02-24 21:29:15 +0100

    feat: favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -6,6 +6,7 @@ const Main = ({ children, router}) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.gif" sizes="any" />
         <title>Sergio Aramburu</title>
       </Head>
 
