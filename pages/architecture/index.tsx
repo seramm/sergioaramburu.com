d4df8fff01ad6c39960e09dbee0e33a8dd5097e7
seramm <sergioaramburu@tutanota.com>
2024-10-01 18:11:34 +0200

    feat: working /architecture/

---
+++ pages/architecture/index.tsx
@@ -0,0 +1,17 @@
+import { Box, Image } from "@chakra-ui/react";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
+export default function Page() {
+  return (
+    <Box>
+      <NavBar />
+      <Box marginX="auto" px={100}>
+        <Box display={{ md: "flex" }} py={9}>
+          <Image src="/images/architecture.png" rounded="2xl" />
+        </Box>
+      </Box>
+      <Footer />
+    </Box>
+  );
+}
