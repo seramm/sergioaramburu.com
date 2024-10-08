823c6584a9fb9edc755f082c45c79c54b28236a1
seramm <sergioaramburu@tutanota.com>
2024-10-08 11:59:34 +0200

    feat: link to architecture image

--- pages/architecture/index.tsx
+++ pages/architecture/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Image } from "@chakra-ui/react";
 import Footer from "components/footer";
 import NavBar from "components/navbar";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -8,11 +9,13 @@ export default function Page() {
       <NavBar />
       <Box marginX="auto" px={100}>
         <Box display={{ md: "flex" }} py={9}>
-          <Image
-            alt="Homelab infrastructure"
-            src="/images/architecture.png"
-            rounded="2xl"
-          />
+          <Link href="/images/architecture.png">
+            <Image
+              alt="Homelab infrastructure"
+              src="/images/architecture.png"
+              rounded="2xl"
+            />
+          </Link>
         </Box>
       </Box>
       <Footer />
