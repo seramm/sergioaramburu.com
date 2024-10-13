5718679bfc16b0b698ed0003e4eeeba6ac7ffb63
seramm <sergioaramburu@tutanota.com>
2024-10-13 13:26:49 +0200

    chore: use defined container

--- pages/architecture/index.tsx
+++ pages/architecture/index.tsx
@@ -1,24 +1,19 @@
 import { Box, Image } from "@chakra-ui/react";
-import Footer from "components/footer";
-import NavBar from "components/navbar";
+import Container from "components/container";
 import Link from "next/link";
 
 export default function Page() {
   return (
-    <Box>
-      <NavBar />
-      <Box marginX="auto" px={100}>
-        <Box display={{ md: "flex" }} py={9}>
-          <Link href="/images/architecture.png">
-            <Image
-              alt="Homelab infrastructure"
-              src="/images/architecture.png"
-              rounded="2xl"
-            />
-          </Link>
-        </Box>
+    <Container>
+      <Box display={{ md: "flex" }} py={9}>
+        <Link href="/images/architecture.png">
+          <Image
+            alt="Homelab infrastructure"
+            src="/images/architecture.png"
+            rounded="2xl"
+          />
+        </Link>
       </Box>
-      <Footer />
-    </Box>
+    </Container>
   );
 }
