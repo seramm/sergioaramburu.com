c6db0d7fd2d53ec9c943cdb0a335cf30dbb78cea
seramm <seram@tuta.com>
2025-03-24 10:01:18 +0100

    feat: API Checker in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,7 @@
-import { Box, Heading, HStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Spacer, Spinner, Text } from "@chakra-ui/react";
 import LinkItem from "./link-item";
 import siteConfig from "data";
+import { useEffect, useState } from "react";
 import { GitHubIcon, LinkedInIcon } from "./icons";
 
 export default function Footer() {
@@ -19,8 +20,36 @@ export default function Footer() {
           <LinkItem href={siteConfig.profiles.linkedin} icon={LinkedInIcon}>
             LinkedIn
           </LinkItem>
+          <Spacer />
+          <Box>
+            <HStack>
+              <Text>API: </Text>
+              <ApiCheck />
+            </HStack>
+          </Box>
         </HStack>
       </Box>
     </Box>
   );
+}
+function ApiCheck() {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetch("https://sergioaramburu.com/api/")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data.status);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  if (!posts)
+    return (
+      <Text>
+        <Spinner size="xs" /> Checking...
+      </Text>
+    );
 }
