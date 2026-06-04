2230be4b45e61a2cf90aeeab61dcb378e92a8c6c
seramm <seram@tuta.com>
2026-06-04 17:48:52 +0300

    chore: remove eslint config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   reactStrictMode: true,
   output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   i18n,
 };
